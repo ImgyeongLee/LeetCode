35e2f47a820fe41675c5c98e09d9604d878d4fef
ImgyeongLee <mingbab33@gmail.com>
2023-11-12 21:17:52 -0800

    Coding assessment study

--- Array (MS)/217_Contains_Duplicate.py
+++ Array (MS)/217_Contains_Duplicate.py
@@ -6,19 +6,3 @@ class Solution:
                 return True
             record.add(num)
         return False
-
-
-    def isAnagram(self, s: str, t: str) -> bool:
-        count = [0] * 26
-
-        for c in s:
-            count[ord(c) - ord('a')] += 1
-
-        for c in t:
-            count[ord(c) - ord('a')] -= 1
-
-        for val in count:
-            if val != 0:
-                return False
-
-        return True
