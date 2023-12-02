81828ac3fc62210c8884f4f528a13139fbcad2a8
ImgyeongLee <mingbab33@gmail.com>
2023-12-01 23:57:45 -0800

    Worked on Programmers Problems (PDT: Dec 1, 2023)

--- Programmers/solution3.py
+++ Programmers/solution3.py
@@ -30,10 +30,67 @@ def solution4(s):
             return False
     return False
 
-
+# 행렬의 덧셈
 def solution5(arr1, arr2):
-    pass
+    result = arr1
+    biglen = len(arr1)
+    length = len(arr1[0])
+    for i in range(biglen):
+        for j in range(length):
+            result[i][j] += arr2[i][j]
+    return result
 
+# 행렬의 덧셈
+def solution6(arr1, arr2):
+    result = arr1
+    biglen = len(arr1)
+    length = len(arr1[0])
+    for i in range(biglen):
+        for j in range(length):
+            result[i][j] += arr2[i][j]
+    return result
+
+# 최대공약수와 최소공배수
+def solution7(n, m):
+    answer = []
+    return answer
+
+# 이상한 문자 만들기
+def solution8(s):
+    count = 0
+    word = ""
+    for c in s:
+        if ord(c) == 32:
+            count = 0
+
+        if ord(c) >= 65 and ord(c) <= 90:
+            count += 1
+        elif ord(c) >= 97 and ord(c) <= 122:
+            count += 1
+
+        if count % 2 == 1:
+            word += c.upper()
+        else:
+            word += c.lower()
+    return word
+
+# 시저 암호
+def solution9(s, n):
+    password = ""
+    for c in s:
+        if ord(c) == 32:
+            password += c
+            continue
+        cizer= ord(c) + n
+        if ord(c) >= 97 and ord(c) <= 122 and cizer > 122:
+            cizer = 96 + n
+        elif ord(c) >= 65 and ord(c) <= 90 and cizer > 90:
+            cizer = 64 + n
+
+        password += chr(cizer)
+
+    return password
 
 if __name__ == "__main__":
-    print([1, 2] + [1, 4])
+    print(ord('Z'))
+    print(solution9("a", 4))
