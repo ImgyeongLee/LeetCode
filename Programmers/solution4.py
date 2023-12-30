990b0d72b13347b4860e76325ffa09dec5c694f8
ImgyeongLee <mingbab33@gmail.com>
2023-12-30 14:03:47 +0900

    Programmers

--- Programmers/solution4.py
+++ Programmers/solution4.py
@@ -1,3 +1,22 @@
 # 문자열 내 마음대로 정렬하기
 def solution1(strings, n):
     return sorted(strings, key=lambda word:(word[n], word))
+
+
+def solution3(N, stages):
+    player_num = len(stages)
+    data_set = {}
+    for stage in stages:
+        if N < stage:
+            continue
+        if stage in data_set:
+            data_set[stage] += 1
+        else:
+            data_set[stage] = 1
+
+
+    return data_set
+
+
+if __name__ == "__main__":
+    print(solution3(2, [2, 1, 2, 6, 2, 4, 3, 3]))
